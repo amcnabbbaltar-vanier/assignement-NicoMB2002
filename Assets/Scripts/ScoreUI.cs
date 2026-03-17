56c1dc9689638b9e8ca0f2760214739bb19fc132
NicoMB2002 <nicomoncayo@hotmail.com>
2026-03-17 00:06:15 -0400

    Added HUD for health and Score

---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class ScoreUI : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+
+    void Update()
+    {
+        scoreText.text = "Score: " + GameManager.Instance.score;
+    }
+
+}
